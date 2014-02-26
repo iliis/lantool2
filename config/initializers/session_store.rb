ade1f0a1a26cb9ca0bc2f1c23d132916bb5be451
iliis <samuelbryner@gmx.ch>
2014-02-26 16:58:47 +0100

    Set session timeout to 3 days. Should fix #5.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-LanTool2::Application.config.session_store :cookie_store, key: '_LanTool2_session'
+LanTool2::Application.config.session_store :cookie_store, key: '_LanTool2_session', :expire_after => 3.days
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
